ed157b9fb3a5ff5c6ad2bb6f05463170c2686011
rey301 <abrahamrey3001@gmail.com>
2024-06-16 10:37:16 +0100

    Update game functionality with ties

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const gameController = (function() {
-	const gameBoard = (function() {
+	let gameBoard = resetGameBoard();
+	let gridStr = '';
+
+	function resetGameBoard() {
 		let gameBoard = new Array();
 		for (let i=0; i < 9; i++) {
 			gameBoard[i] = ' ';
 		}
 		return gameBoard;
-	}());
-
-	let gridStr = '';
+	}
 
 	function updateGridStr() {
 		gridStr = '';
@@ -20,29 +21,38 @@ const gameController = (function() {
 		return gridStr;
 	};
 	
-
-
 	function checkWin() {
-		const winCondArr = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
+		const winCondArr = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],
+							[0,4,8],[6,4,2]];
+		let tieFlag = true;
 		for (let i = 0; i < 8; i++) {
 			let index1 = winCondArr[i][0];
 			let index2 = winCondArr[i][1];
 			let index3 = winCondArr[i][2];
 
-			if (gameBoard[index1] == 'x' && gameBoard[index2] == 'x'
-				&& gameBoard[index3] == 'x') {
-					return 'x wins';
-			} else if (gameBoard[index1] == 'o' && gameBoard[index2] == 'o'
-				&& gameBoard[index3] == 'o') {
-					return 'o wins';
-			} 
-		}
+			// check if any spaces are empty
+			if (gameBoard[i] == ' ') {
+				tieFlag = false;
+			}
 
-		return false;
+			if (gameBoard[index1] == 'x' && gameBoard[index2] == 'x'
+					&& gameBoard[index3] == 'x') {
+				return 'x wins';
+			} else if (gameBoard[index1] == 'o' && gameBoard[index2] == 'o'
+					&& gameBoard[index3] == 'o') {
+				return 'o wins';
+			}
+		}
+		
+		if (tieFlag) {
+			return "It's a tie!";
+		} else {
+			return false;
+		}
 	}
 
 	function compRound(symbol) {
-		let randomNum = Math.floor(Math.random() * 8);
+		let randomNum = Math.floor(Math.random() * 9);
 		while (gameBoard[randomNum] !== ' ') {
 			randomNum = Math.floor(Math.random() * 9);
 		}
@@ -55,8 +65,8 @@ const gameController = (function() {
 		return gameBoard;
 	};
 	
-	function playerRound(pos) {
-		gameBoard[pos] = 'x'; 
+	function playerRound(pos, symbol) {
+		gameBoard[pos] = symbol; 
 		updateGridStr();
 		console.log(gridStr);
 
@@ -64,17 +74,24 @@ const gameController = (function() {
 	};
 
 	function playGame() {
+		gameBoard = resetGameBoard();
 		let input = prompt('X or O?');
 		let playerSymbol = input.toLowerCase();
 		let compSymbol;
 		playerSymbol == 'x' ? compSymbol = 'o' : compSymbol = 'x';
 
 		console.log('Game Start!');
+		console.log(gameController.updateGridStr());
 
-		while (!checkWin()) {
+		let won = checkWin();
+
+		while(!checkWin()) {
 			compRound(compSymbol);
-			let posInput = prompt('Which position?');
-			playerRound(posInput);
+			if (checkWin()) {
+				break;
+			}
+			posInput = prompt('Which position?');
+			playerRound(posInput, playerSymbol);
 		}
 
 		return checkWin();
@@ -87,5 +104,3 @@ const gameController = (function() {
 		playGame,
 	};
 })();
-
-console.log(gameController.updateGridStr());
